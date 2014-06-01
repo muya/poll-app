3d27bed0ff48a0a161716a286a1a81f4162715cb
muya <kingkonig@gmail.com>
2014-06-01 16:27:03 +0300

    part 2 complete

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from polls.models import Poll, Choice
 
+class ChoiceInline(admin.TabularInline):
+	"""docstring for ChoiceInline"""
+	model = Choice
+	extra = 3
+		
+
+class PollAdmin(admin.ModelAdmin):
+	"""docstring for PollAdmin"""
+	fieldsets = [
+		(None, {'fields': ['question']}),
+		('Date Information', {'fields': ['pub_date']}),
+	]
+	inlines = [ChoiceInline]
+	list_display = ('question', 'pub_date', 'was_published_recently')
+
+	list_filter = ['pub_date']
+
+	search_fields = ['question']
+
+admin.site.register(Poll, PollAdmin)
+
+		
 # Register your models here.
